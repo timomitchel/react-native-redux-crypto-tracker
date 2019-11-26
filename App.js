646df7a96d81d2769f83d0f1829168b0ff2b9131
Timo Tyrrell <timomitchel23@gmail.com>
2019-11-26 13:52:37 -0700

    Add header and update dependencies

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Header } from './src/components';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Header/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  headerContainer: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
